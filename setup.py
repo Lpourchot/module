ec84a8e07382e4949bae43c2d3c3c29834b2f00d
Laurent Pourchot <laurent_pourchot@fr.ibm.com>
2024-07-05 10:27:18 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,14 @@ from setuptools import setup
 
 setup(name='new_module',
       version='0.1',
-      description='xxx',
+      description='A simple library for encoding and decoding pandas DataFrame.',
       url='https://github.com/Lpourchot/new_module',
-      author='Pourchot',
+      author='Laurent Pourchot',
       author_email='laurent_pourchot@fr.ibm.com',
-      license='MIT',
-      packages=['src'],
+      license='GNU General Public License v3.0',
+      packages=['new_module'],
+      install_requires=[
+          'numpy',
+          'pandas'
+      ],
       zip_safe=False)
