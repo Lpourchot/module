cf5fa5b9b3b3ccfc033601c5c6e00925d67d4742
Laurent Pourchot <laurent_pourchot@fr.ibm.com>
2024-07-05 09:57:14 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='module',
+    version='1.0.0',
+    url='https://github.com/Lpourchot/module.git',
+    author='Laurent Pourchot',
+    author_email='laurent_pourchot@fr.ibm.com',
+    description='module selon les besoins',
+    packages=find_packages(),    
+    install_requires=['numpy >= 1.11.1'],
+)
+    install_requires=read_requirements("requirements.txt"),
+    entry_points={
+        "console_scripts": ["project_name = project_name.__main__:main"]
+    },
+    extras_require={"test": read_requirements("requirements-test.txt")},
+)
